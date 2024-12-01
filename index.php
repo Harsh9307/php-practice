883a829431bbf02c12e0c9974577673135d80017
Harsh9307 <diwaseharsh@gmail.com>
2024-12-01 10:28:37 +0530

    first code

---
+++ index.php
@@ -0,0 +1,36 @@
+<!-- <!DOCTYPE html>
+<html>
+<body>
+
+<h1>
+    <?php
+        $x = 10;
+        $y = 5;
+
+        echo $x. ' , ' .$y;
+
+        echo $x*$y;
+    ?>
+    <?= 'Hello World' ?>
+
+</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html> -->
+
+
+
+<form method="post">
+    <input type="text" name="x" placeholder="Enter value for x">
+    <input type="text" name="y" placeholder="Enter value for y">
+    <button type="submit">Submit</button>
+</form>
+
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $x = $_POST['x'];
+        $y = $_POST['y'];
+        echo "x: $x, y: $y, Product: " . ($x * $y);
+    }
+?>
