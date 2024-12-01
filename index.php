fe75aaedb4f590425619ba878161b88c5064895d
Harsh9307 <diwaseharsh@gmail.com>
2024-12-01 15:57:09 +0530

    code

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <body>
 
@@ -17,7 +17,7 @@
 <p>My first paragraph.</p>
 
 </body>
-</html> -->
+</html>
 
 
 
@@ -57,7 +57,32 @@
     for ($i = 1; $i <= $x; $i++) {
         echo "$i x $y = " . ($i * $y) . "<br>";
     }
+
+    function multiply($a, $b) {
+        return $a * $b;
+    }
+    echo "Result: " . multiply(10, 20)."<br>";
+
+    $numbers = [1, 2, 3, 4, 5];
+    foreach ($numbers as $num) {
+        echo "Square of $num: " . ($num ** 2) . "<br>";
+    }
+
+
 ?>
+<style>
+    h1 {
+        color: blue;
+    }
+    .dynamic-output {
+        font-weight: bold;
+    }
+</style>
+<div class="dynamic-output">
+    <?php echo "Styled Output: " . ($x * $y); ?>
+</div>
+
+
 
 
 
